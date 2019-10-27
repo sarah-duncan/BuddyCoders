83dfcff09211cf51e1375a4227ac7ffdcb440254
sarah-duncan <55993775+sarah-duncan@users.noreply.github.com>
2019-10-27 19:55:23 +0000

    Add files via upload
    This is the code from the first session of Buddy Coders. It takes the inputted number and determines whether it is a prime number. Written in C.

---
+++ prime_numbers.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+/* Declare function*/
+float input();
+int main(void) {
+	int number;
+	/*float f;
+	printf("Please input a a positive integer: ");
+	scanf("%f", &f);*/
+
+	/* Checks if f is an integer*/
+	float f = input();
+	number = f;
+	printf("You have inputted: %d", number);
+	if( f!= number)
+	{ 
+		printf("You have not entered an integer, please try again.\n");
+		f = input();
+	}
+	if(f==1)
+	{
+		printf("1 is not a prime number");
+	}
+	else
+	{
+		if(number>0)
+		{
+		 int i;
+		 int factors = 0;
+			for ( i=1; i<= number; i++)
+			{  double check = number % i;
+				if (check==0)
+				{
+					++factors;
+				if (factors>2){
+					printf("This is not a prime number");
+					return 0;
+				}
+				}
+			}
+			printf("This is a prime number");
+		}
+		else 
+		{
+			printf("This is not a positive number");
+		}
+	}
+	return 0;
+}
+float input(void){
+	float f;
+	printf("Please input a a positive integer: ");
+	scanf("%f", &f);
+	return f;
+}
